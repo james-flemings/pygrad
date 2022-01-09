209b86d0dbef886d51d5d961c0a8eacf269b9379
james-flemings <jamesflemings9898@gmail.com>
2022-01-08 16:27:07 -0900

    Run through mnist

--- Makefile
+++ Makefile
@@ -1,15 +1,20 @@
 CFLAGS = -Iinclude -O3 -fopenmp --std=c++17 
 CC = g++
 TARGET = examples
-SRC = $(TARGET)/iris-example.cpp $(wildcard src/*.cpp)
-OBJ = $(SRC:.cpp = .o) 
+SRC_IRIS = $(TARGET)/iris-example.cpp $(wildcard src/*.cpp)
+OBJ_IRIS = $(SRC_IRIS:.cpp = .o) 
+SRC_MNIST = $(TARGET)/mnist-example.cpp $(wildcard src/*.cpp)
+OBJ_MNIST = $(SRC_MNIST:.cpp = .o) 
 
 .PHONY: all clean
 
-all: $(TARGET)/iris-example
+all: $(TARGET)/iris-example $(TARGET)/mnist-example
 
-$(TARGET)/iris-example: $(OBJ)
-	$(CC) $(CFLAGS) -o $(TARGET)/iris-example $(OBJ)
+$(TARGET)/iris-example: $(OBJ_IRIS)
+	$(CC) $(CFLAGS) -o $(TARGET)/iris-example $(OBJ_IRIS)
+
+$(TARGET)/mnist-example: $(OBJ_MNIST)
+	$(CC) $(CFLAGS) -o $(TARGET)/mnist-example $(OBJ_MNIST)
 
 clean: 
 	rm -f src/*.o
--- examples/mnist-example.cpp
+++ examples/mnist-example.cpp
@@ -1,6 +1,5 @@
 #include "../eigen/Eigen/Eigen"
 #include "../mnist/include/mnist/mnist_reader.hpp"
-#include "../mnist/include/mnist/mnist_utils.hpp"
 #include "Model.h"
 
 #include <iostream>
@@ -10,35 +9,44 @@ std::string MNIST_DATA_LOCATION = "../mnist";
 
 using namespace Eigen;
 
+typedef std::vector<std::tuple<VectorXd, VectorXd>> data;
+
 void readMnist(data &train_data, data &valid_data);
 
 int main(int argc, char *argv[]) {
   data train_data, valid_data;
-  VectorXd x, y;
   readMnist(train_data, valid_data);
-  auto [x, y] = train_data[0];
-  std::cout << "X: " << std::endl;
-  std::cout << x << std::endl;
-  std::cout << "Y: " << std::endl;
-  std::cout << y << std::endl;
+
+  std::vector<std::unique_ptr<Layer>> layers;
+  layers.push_back(std::make_unique<DenseLayer>(30, 784, "Sigmoid"));
+  layers.push_back(std::make_unique<DenseLayer>(10, 0, "Sigmoid"));
+  Model model(layers);
+  model.summary();
+
+  model.train(train_data, 30, 10, 2.0, 0, valid_data);
+
   return 0;
 }
 
 void readMnist(data &train_data, data &valid_data) {
-  mnist::MNIST_dataset<std::vector, std::vector<uint8_t>, uint8_t> dataset =
-      mnist::read_dataset<std::vector, std::vector, uint8_t, uint8_t>(
+  mnist::MNIST_dataset<std::vector, std::vector<double>, uint8_t> dataset =
+      mnist::read_dataset<std::vector, std::vector, double, uint8_t>(
           MNIST_DATA_LOCATION);
   for (int i = 0; i < dataset.training_images.size(); i++) {
-    VectorXd label(10);
+    VectorXd label;
+    label.setZero(10);
     label(dataset.training_labels[i]) = 1.0;
-    VectorXd data(dataset.training_images[i].data());
+    Map<VectorXd> data(dataset.training_images[i].data(),
+                       dataset.training_images[i].size());
     data = data.array() * (double)1 / 255;
     train_data.push_back(std::make_tuple(data, label));
   }
   for (int i = 0; i < dataset.test_images.size(); i++) {
-    VectorXd label(10);
+    VectorXd label;
+    label.setZero(10);
     label(dataset.test_labels[i]) = 1.0;
-    VectorXd data(dataset.test_images[i].data());
+    Map<VectorXd> data(dataset.test_images[i].data(),
+                       dataset.test_images[i].size());
     data = data.array() * (double)1 / 255;
     valid_data.push_back(std::make_tuple(data, label));
   }
